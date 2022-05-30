e3fafcc4ebfc15b22db133d3fb89c174b9f43cd6
iLynette <lynetteacholah@icloud.com>
2022-05-30 17:04:12 +0300

    Set up in the constructor method

--- book.rb
+++ book.rb
@@ -1,2 +1,8 @@
 class Book
     attr_accessor :publisher, :cover_state
+
+    def initialize(publisher, cover_state)
+        @publisher = publisher
+        @cover_state = cover_state
+    end
+end
