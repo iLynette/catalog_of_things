7b377f50c627c11d25c9396f4d3d36c6ba406375
daudi13 <davouma13@gmail.com>
2022-05-30 21:17:51 +0300

    Add Genre class initialize

--- genre.rb
+++ genre.rb
@@ -1,3 +1,11 @@
 class Genre < Item
-  attr_reader :items
+  attr_accessor :name
+  attr_reader :items, :id
+
+  def initialize(name)
+    super(archived:false, publish_date, id)
+    @items = []
+    @id = id.Random.rand(1..1000)
+    @name = name
+  end
 end
