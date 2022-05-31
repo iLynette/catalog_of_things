4e2f7ccf54d1dfc13d4ec489cc91a83e0b5cec08
daudi13 <davouma13@gmail.com>
2022-05-31 11:47:22 +0300

    remove class genre

--- genre.rb
+++ genre.rb
@@ -1,16 +1,2 @@
 class Genre
-  attr_accessor :name
-  attr_reader :items, :id
-
-  def initialize(name, id: Random.rand(1..1000))
-    super()
-    @items = []
-    @id = id
-    @name = name
-  end
-
-  def add_item(item)
-    @items << item
-    item.genre = self
-  end
 end
