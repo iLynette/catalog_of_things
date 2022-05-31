41050c3f42c594efec728c64918f44ba8865f2b0
daudi13 <davouma13@gmail.com>
2022-05-31 13:56:21 +0300

    Add add_item() method in class Genre

--- genre.rb
+++ genre.rb
@@ -1,11 +1,16 @@
 class Genre
   attr_accessor :name
-  attr_reader :item, :id
+  attr_reader :items, :id
 
-  def initialize(name, id: rand(1..100_000)){
+  def initialize(name, id: rand(1..100_000))
     super()
     @id = id
-    @item = []
+    @items = []
     @name = name
-  }
+  end
+
+  def add_item(item)
+    @items << item
+    item.genre = self
+  end
 end
