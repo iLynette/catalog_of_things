e18168741cf0be321b29f286939a7ba0d9309e3a
iLynette <lynetteacholah@icloud.com>
2022-05-31 20:16:08 +0300

    Test if it returns an instance of Book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,4 +5,10 @@ describe Book do
   before :each do
     @book = Book.new 'Publisher', 'Cover state', 'Publish date'
   end
+
+  describe '#new' do
+    it 'takes two parameters and returns a Book object' do
+      @book.should be_an_instance_of Book
+    end
+  end
 end
