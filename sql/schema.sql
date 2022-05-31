8b5fe3a1f7b46b5a1966573b829083252581af61
iLynette <lynetteacholah@icloud.com>
2022-05-31 15:43:26 +0300

    Leave foreign key and references for label

--- sql/schema.sql
+++ sql/schema.sql
@@ -14,8 +14,6 @@ CREATE TABLE book(
   publish_date DATE,
   archived BOOLEAN,
   label_id  INT,
-  FOREIGN KEY (genre_id) REFERENCES genre(id),
-  FOREIGN KEY (author_id) REFERENCES author(id),
   FOREIGN KEY (label_id) REFERENCES label(id),
   PRIMARY KEY(id)
 );
