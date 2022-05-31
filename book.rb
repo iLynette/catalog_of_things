8a837b5b88718f0ef677241749796d2ec95375f5
iLynette <lynetteacholah@icloud.com>
2022-05-31 13:53:22 +0300

    Fix linter errors

--- book.rb
+++ book.rb
@@ -1,15 +1,15 @@
 require './item'
 
 class Book < Item
-    attr_accessor :publisher, :cover_state
+  attr_accessor :publisher, :cover_state
 
-    def initialize(publisher, cover_state, publish_date)
-        super(publish_date, true)
-        @publisher = publisher
-        @cover_state = cover_state
-    end
+  def initialize(publisher, cover_state, publish_date)
+    super(publish_date, true)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
 
-    def can_be_archived?
-        super || cover_state == 'bad'
-    end
+  def can_be_archived?
+    super || cover_state == 'bad'
+  end
 end
