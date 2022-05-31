1b0775d6fc6a50f196294f47e57213b4114b5427
iLynette <lynetteacholah@icloud.com>
2022-05-31 15:38:52 +0300

    Add books table

--- sql/schema.sql
+++ sql/schema.sql
@@ -6,3 +6,16 @@ CREATE TABLE label(
  color VARCHAR(50),
  PRIMARY KEY(id)
 );
+
+CREATE TABLE book(
+  id INT,
+  publisher VARCHAR(50),
+  cover_state VARCHAR(15),
+  publish_date DATE,
+  archived BOOLEAN,
+  label_id  INT,
+  FOREIGN KEY (genre_id) REFERENCES genre(id),
+  FOREIGN KEY (author_id) REFERENCES author(id),
+  FOREIGN KEY (label_id) REFERENCES label(id),
+  PRIMARY KEY(id)
+);
