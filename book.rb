ab3304bf6d0ad2cdbe9c134a8cde68fecc5ca902
iLynette <lynetteacholah@icloud.com>
2022-05-31 13:52:31 +0300

    Add publish_date as a constructor

--- book.rb
+++ book.rb
@@ -1,7 +1,10 @@
-class Book
+require './item'
+
+class Book < Item
     attr_accessor :publisher, :cover_state
 
-    def initialize(publisher, cover_state)
+    def initialize(publisher, cover_state, publish_date)
+        super(publish_date, true)
         @publisher = publisher
         @cover_state = cover_state
     end
