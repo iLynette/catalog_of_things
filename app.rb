67a7fed19f7f84435df6e61e156bf5fc227a84c9
daudi13 <davouma13@gmail.com>
2022-06-01 08:07:35 +0300

    initialize App class

--- app.rb
+++ app.rb
@@ -8,5 +8,18 @@ require './genre'
 
 
 class App
-  
+  attr_accessor :user_input
+
+  def initialize
+    puts 'Welcome to the catalog of things app'
+    prompt
+    @books = []
+    @music_album = []
+    @movies = []
+    @games = []
+    @genre = []
+    @labels = []
+    @authors =[]
+    @user_input = gets.chomp
+  end
 end
