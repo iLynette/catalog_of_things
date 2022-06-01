c0f454cfb638c84ea52d2b0bbd11c7db7bd1dea7
daudi13 <davouma13@gmail.com>
2022-06-01 18:06:42 +0300

    add read_music method in Storage module

--- storage.rb
+++ storage.rb
@@ -3,6 +3,9 @@ require 'json'
 module Storage
   def read_music
     albums = []
-    JSON.parse(File.read())
+    JSON.parse(File.read(./data/music.json)).each do |music|
+      albums.push(Music.new(music['publish_date'], music['on_spotify'], music['archived']))
+    end
+    albums
   end
 end
