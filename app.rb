27e0861c5ae10d25ae8254424d91d9309266f8cc
daudi13 <davouma13@gmail.com>
2022-06-01 08:08:37 +0300

    Add prompt method

--- app.rb
+++ app.rb
@@ -22,4 +22,24 @@ class App
     @authors =[]
     @user_input = gets.chomp
   end
+
+  def prompt
+    puts '
+    1 List all books
+    2 List all music albums
+    3 List all movies
+    4 List of games
+    5 List all genres
+    6 List all labels
+    7 List all authors
+    8 List all sources
+    9 Add a book
+    10 Add a music album
+    11 Add a movie
+    12 Add a game
+    13 exit app
+    '
+    puts ' '
+    puts 'select an option: '
+  end
 end
