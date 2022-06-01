e72571a2c29cb2f6b7a87662b117f0ea7774fd75
daudi13 <davouma13@gmail.com>
2022-06-01 15:05:07 +0300

    refactor code to accomodate app.rb

--- music_album.rb
+++ music_album.rb
@@ -1,13 +1,15 @@
 require './item'
+require 'date'
 
 class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(publish_date, on_spotify: false, archived: false)
-    super(archived, publish_date)
+  def initialize(on_spotify, publish_date, archived)
+    super(publish_date, archived)
     @on_spotify = on_spotify
   end
 
+  private
   def can_be_archived
     super && on_spotify
   end
