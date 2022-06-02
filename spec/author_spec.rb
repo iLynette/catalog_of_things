b1c69f3391f19fdd87eca610b7fe31a74c79bd50
Tufahel <tufahel96@gmail.com>
2022-06-02 18:48:20 +0600

    fixing linters issues

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,34 +1,34 @@
-require_relative '../author.rb'
-require_relative '../item.rb'
+require_relative '../author'
+require_relative '../item'
 
 describe Author do
-    before :each do
-        @author = Author.new('Tufahel', 'Ahmed')
+  before :each do
+    @author = Author.new('Tufahel', 'Ahmed')
+  end
+
+  context 'initialize method' do
+    it 'author should be an instance of Author' do
+      expect(@author).to be_an_instance_of Author
     end
 
-    context 'initialize method' do
-        it 'author should be an instance of Author' do
-            expect(@author).to be_an_instance_of Author
-        end
-
-        it 'should have a first_name instance variable' do
-            name = @author.first_name
-            expect(name).to eq 'Tufahel'
-        end
-
-        it 'should have a last_name instance variable' do
-            name = @author.last_name
-            expect(name).to eq 'Ahmed'
-        end
+    it 'should have a first_name instance variable' do
+      name = @author.first_name
+      expect(name).to eq 'Tufahel'
     end
 
-    context 'add item' do
-        it 'should add an item' do
-            author = Author.new('Tufahel', 'Ahmed')
-            item = Item.new('2021', false)
-            author.add_item(item)
-            expect(author.items). to include(item)
-            expect(item.author).to be(author)
-        end
+    it 'should have a last_name instance variable' do
+      name = @author.last_name
+      expect(name).to eq 'Ahmed'
     end
+  end
+
+  context 'add item' do
+    it 'should add an item' do
+      author = Author.new('Tufahel', 'Ahmed')
+      item = Item.new('2021', false)
+      author.add_item(item)
+      expect(author.items).to include(item)
+      expect(item.author).to be(author)
+    end
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,27 +1,27 @@
-require_relative '../game.rb'
+require_relative '../game'
 
 describe Game do
-    before :each do
-        @game = Game.new(true, '2019', '2011', false)
-    end
+  before :each do
+    @game = Game.new(true, '2019', '2011', false)
+  end
 
-    context 'initialize method' do
-        it 'game is an instance of Game' do
-            expect(@game).to be_an_instance_of Game
-        end
-        it 'should have multiplayer instance variable' do
-            expect(@game.multiplayer).to eq true
-        end
-        it 'should have last_played_at instance variable' do
-            expect(@game.last_played_at.to_s).to eq '2019'
-        end
+  context 'initialize method' do
+    it 'game is an instance of Game' do
+      expect(@game).to be_an_instance_of Game
     end
+    it 'should have multiplayer instance variable' do
+      expect(@game.multiplayer).to eq true
+    end
+    it 'should have last_played_at instance variable' do
+      expect(@game.last_played_at.to_s).to eq '2019'
+    end
+  end
 
-    context 'can_be_archived? method' do
-        it 'returns true if parent return true' do
-            game = Game.new(true, (Time.now.year - 4), (Time.now.year - 12), false)
-            result = game.send(:can_be_archived?)
-            expect(result).to be_truthy
-        end
+  context 'can_be_archived? method' do
+    it 'returns true if parent return true' do
+      game = Game.new(true, (Time.now.year - 4), (Time.now.year - 12), false)
+      result = game.send(:can_be_archived?)
+      expect(result).to be_truthy
     end
+  end
 end
