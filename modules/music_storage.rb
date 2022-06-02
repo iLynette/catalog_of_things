0e0460b420085020764d4aeb24976adb836410ab
daudi13 <davouma13@gmail.com>
2022-06-02 18:52:03 +0300

    Add conditional when file is empty

--- modules/music_storage.rb
+++ modules/music_storage.rb
@@ -4,9 +4,13 @@ require './music_album'
 module Storage
   def read_music
     albums = []
+    if File.zero?('./data/music.json')
+      print ''
+      else
     JSON.parse(File.read('./data/music.json')).each do |music|
       albums.push(MusicAlbum.new(music['album_name'], music['on_spotify'], music['publish_date'], music['archived']))
     end
+  end
     albums
   end
 
