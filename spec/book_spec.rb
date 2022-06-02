dd894801d64ff2c5ffc157b5e9d84039fa739aee
iLynette <lynetteacholah@icloud.com>
2022-06-02 08:44:06 +0300

    Remove commented code

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -5,28 +5,4 @@ describe Book do
   before :each do
     @book = Book.new 'Publisher', 'Cover state', 'Publish date'
   end
-
-  describe '#new' do
-    it 'takes two parameters and returns a Book object' do
-      @book.should be_an_instance_of Book
-    end
-  end
-
-  describe '#publisher' do
-    it 'detects the right publisher' do
-      expect(@book.publisher).to eq 'Publisher'
-    end
-  end
-
-  describe '#cover_state' do
-    it 'detects the right cover_state' do
-      expect(@book.cover_state).to eq 'Cover state'
-    end
-  end
-
-  describe '#publish_date' do
-    it 'detects the right publish_date' do
-      expect(@book.publish_date).to eq 'Publish date'
-    end
-  end
 end
