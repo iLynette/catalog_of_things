997635fcad57df8d9dbd21317c513db441433d1a
Tufahel <tufahel96@gmail.com>
2022-06-02 17:32:13 +0600

    last_played_at instance vaiable check

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -12,5 +12,8 @@ describe Game do
         it 'should have multiplayer instance variable' do
             expect(@game.multiplayer).to eq true
         end
+        it 'should have last_played_at instance variable' do
+            expect(@game.last_played_at.to_s).to eq '2019'
+        end
     end
 end
