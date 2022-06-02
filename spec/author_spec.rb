5ead2e1460d43be307adb8ae84f228e3f18851ca
Tufahel <tufahel96@gmail.com>
2022-06-02 18:40:43 +0600

    should add an item

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../author.rb'
+require_relative '../item.rb'
 
 describe Author do
     before :each do
@@ -18,6 +19,16 @@ describe Author do
         it 'should have a last_name instance variable' do
             name = @author.last_name
             expect(name).to eq 'Ahmed'
+        end
+    end
+
+    context 'add item' do
+        it 'should add an item' do
+            author = Author.new('Tufahel', 'Ahmed')
+            item = Item.new('2021', false)
+            author.add_item(item)
+            expect(author.items). to include(item)
+            expect(item.author).to be(author)
         end
     end
 end
