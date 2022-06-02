d4df59c1b5d3eb61b177d38c2a7ffe2223f24eba
iLynette <lynetteacholah@icloud.com>
2022-06-02 07:53:16 +0300

    Fix conflict

--- item.rb
+++ item.rb
@@ -15,9 +15,9 @@ class Item
     genre.add_item(self) unless genre.items.include? self
   end
 
-  def label=(label)
+  def add_label(label)
     @label = label
-    label.items.push(self) unless label.items.include?(self)
+    
   end
 
   def move_to_archive
