11b3adbd8aed7ea5eda1d3d8d2ac1a1fc1770b88
daudi13 <davouma13@gmail.com>
2022-06-03 08:20:44 +0300

    Add test for instance of MusicAlbum

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,9 +1,11 @@
-require_relative './music_album.rb'
+require_relative '../music_album'
 
 describe MusicAlbum do
   before :all do
     @music_album = MusicAlbum.new('rap', 2001)
   end
 
-  
+  it 'should be an instance of MusicAlbum class' do
+    expect(@music_album).to be_instance_of MusicAlbum
+  end
 end
