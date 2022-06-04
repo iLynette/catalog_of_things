cd615ce1cfc36946f0137bfa0db6542ea46da639
daudi13 <davouma13@gmail.com>
2022-06-04 15:52:29 +0300

    test for instance of class Source

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -1,9 +1,15 @@
-require_relative './source'
-require_relative './item'
+require_relative '../source'
+require_relative '../item'
 
 describe Source do
   before :all do
     @source = Source.new('HBO')
     @item = Item.new(2021)
+  end
+
+  context 'Class Object instance' do
+    it 'source should be an instance of class Source' do
+      expect(@source).to be_instance_of Source
+    end
   end
 end
