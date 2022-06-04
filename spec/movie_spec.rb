7a56bd86661e28fcd7fc10c63cc5d12a5113dd98
daudi13 <davouma13@gmail.com>
2022-06-04 17:04:38 +0300

    Add test for checking movie_name

--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -8,4 +8,12 @@ describe Movie do
   it 'should be an instance of class Movie' do
     expect(@movie).to be_instance_of Movie
   end
+
+  it 'should return false on silet' do
+    expect(@movie.silet).to be_falsy
+  end
+
+  it 'should return Morbius as movie name' do
+    expect(@movie.movie_name). to eql 'Morbius'
+  end
 end
